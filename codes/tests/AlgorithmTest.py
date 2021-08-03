95ad9aeefb634a510381e0aca098ff117c7e9b32
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-03 08:58:44 -0300

    testando automata_union

--- codes/tests/AlgorithmTest.py
+++ codes/tests/AlgorithmTest.py
@@ -8,65 +8,58 @@ from ..utils.algorithm import automata_union
 
 class AlgorithmTest(unittest.TestCase):
     def test_automata_union(self) -> None:
-        a = State("A")
-        b = State("B")
-        c = State("C")
+        q0 = State("q0")
+        q1 = State("q1")
+        q2 = State("q2")
 
-        symbols = {'0', '1', '&'}
+        symbols = {'a', 'b'}
         transitions = {
-            Transition(a, '0', b),
-            Transition(a, '0', c),
-            Transition(a, '1', a),
-            Transition(a, '&', b),
-            Transition(b, '&', c),
-            Transition(b, '1', b),
-            Transition(c, '0', c),
-            Transition(c, '1', c)
+            Transition(q0, 'a', q2),
+            Transition(q0, 'b', q1),
+            Transition(q1, 'a', q2),
+            Transition(q1, 'b', q2),
+            Transition(q2, 'a', q2),
+            Transition(q2, 'b', q2)
         }
 
-        initial_state = a
-        final_states = {c}
+        initial_state = q0
+        final_states = {q1}
 
         fa_0 = FiniteAutomata(
-            states = {a,b,c},
+            states = {q0, q1, q2},
             symbols = symbols,
             transitions = transitions,
             initial_state = initial_state,
             final_states = final_states
         )
 
-        print(fa_0)
+        q3 = State("q3")
+        q4 = State("q4")
+        q5 = State("q5")
 
+        symbols = {'a', 'b'}
+        transitions = {
+            Transition(q3, 'a', q4),
+            Transition(q3, 'b', q5),
+            Transition(q4, 'a', q5),
+            Transition(q4, 'b', q5),
+            Transition(q5, 'a', q5),
+            Transition(q5, 'b', q5)
+        }
 
-        # actual = fa_0.determinization()
+        initial_state = q3
+        final_states= {q4}
 
-        # p = State("p")
-        # q = State("q")
-        # r = State("r")
+        fa_1 = FiniteAutomata(
+            states = {q3, q4, q5},
+            symbols = symbols,
+            transitions = transitions,
+            initial_state = initial_state,
+            final_states = final_states
+        )
 
-        # symbols = {'a', 'b', 'c', '&'}
-        # transitions = {
-            # Transition(p, '&', p),
-            # Transition(p, '&', q),
-            # Transition(p, 'b', q),
-            # Transition(p, 'c', r),
-            # Transition(q, 'a', p),
-            # Transition(q, 'b', r),
-            # Transition(q, 'c', p),
-            # Transition(q, 'c', q),
-        # }
-
-        # initial_state = p
-        # final_states= {r}
-
-        # fa_1 = FiniteAutomata(
-            # states = {p,q,r},
-            # symbols = symbols,
-            # transitions = transitions,
-            # initial_state = initial_state,
-            # final_states = final_states
-        # )
-
-        # actual = automata_union(fa_0, fa_1)
-        # print(actual)
+        actual = automata_union(fa_0, fa_1)
+        self.assertEqual(actual.get_initial_state().get_name(), 'S')
+        self.assertEqual(actual.get_final_states(), {q1, q4})
+        # TODO: testar transições
         return None
