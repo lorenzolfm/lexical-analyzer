e4f1db6406ce655b6fd9478cd980b2409096030a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-04 14:28:08 -0300

    adaptando para AbstractSyntaxTree

--- utils/model/Node.py
+++ utils/model/Node.py
@@ -1,31 +1,27 @@
-from genericType import T
-
+from typing import Union
 class Node:
-    def __init__(self, value: T = None, left=None, right=None):
+    def __init__(self, value: str = "", left_child = None, right_child = None) -> None:
         self._value = value
-        self._left = left
-        self._right = right
+        self._left_child = left_child
+        self._right_child = right_child
         self._firstpos = None
         self._lastpos = None
         self._followpos = None
+        return None
 
-    def get_value(self) -> T:
+    def get_value(self) -> str:
         return self._value
 
-    def get_left(self):
-        return self._left
+    def get_left_child(self):
+        return self._left_child
 
-    def get_right(self):
-        return self._right
+    def get_right_child(self):
+        return self._right_child
 
-    def set_left(self, node) -> None:
-        self._left = node
+    def set_left_child(self, node) -> None:
+        self._left_child = node
+        return None
 
-    def set_right(self, node) -> None:
-        self._right = node
-
-    def set_value(self, value: T) -> None:
-        self._value = value
-
-    def __repr__(self):
-        return str(self._value)
+    def set_right_child(self, node) -> None:
+        self._right_child = node
+        return None
