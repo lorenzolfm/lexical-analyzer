be6aa3dadf8c8d135b50a00191b6c004baa833f8
Gabriel Carneiro <carneiro.gabr@gmail.com>
2021-08-09 08:38:22 -0300

    refactor tree test

--- tests/AbstractSyntaxTreeTests.py
+++ tests/AbstractSyntaxTreeTests.py
@@ -7,12 +7,13 @@ from utils.model.AbstractSyntaxTree import AbstractSyntaxTree
 class AbstractSyntaxTreeTests(unittest.TestCase):
     def test_create_syntax_tree_from_regex(self) -> None:
         tree: AbstractSyntaxTree = AbstractSyntaxTree("(a|b)*abb")
-        expected: List = ['a','|','b','*','.','a','.','b','.','b','.','#']
+        expected: List = ['a', '|', 'b', '*', '.', 'a', '.', 'b', '.', 'b', '.', '#']
         actual: List = tree.in_order()
         self.assertEqual(actual, expected)
 
         tree = AbstractSyntaxTree("abc(a|b)?abc(c|d)abc")
-        expected = ['a', '.', 'b', '.', 'c', '.', 'a', '|', 'b', '?', '.', 'a', '.', 'b', '.', 'c', '.', 'c', '|', 'd', '.', 'a', '.', 'b', '.', 'c', '.', '#']
+        expected = ['a', '.', 'b', '.', 'c', '.', 'a', '|', 'b', '?', '.', 'a',
+                    '.', 'b', '.', 'c', '.', 'c', '|', 'd', '.', 'a', '.', 'b', '.', 'c', '.', '#']
         actual = tree.in_order()
         self.assertEqual(actual, expected)
         return None
