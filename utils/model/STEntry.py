7e93f3c17e5a9d3c28aa4db2b798a7516358bfee
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-09 18:36:33 -0300

    feat: primeiro commit para STEntry

---
+++ utils/model/STEntry.py
@@ -0,0 +1,5 @@
+class STEntry:
+    def __init__(self, token: str, lexem: str, position: int):
+        self._token: str = token
+        self._lexem: str = lexem
+        self._position: int = position
