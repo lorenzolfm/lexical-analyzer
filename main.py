04b179815e123d53ef90deadc0e62232cacc9169
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-10 08:32:17 -0300

    main roda controller

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from utils.controller.Controller import Controller
-from utils.model.AbstractSyntaxTree import AbstractSyntaxTree
 
 if __name__ == "__main__":
-    # app = Controller()
-    # app.run()
-    # #.*(b|a).a
-    # tree = AbstractSyntaxTree("a(a|b)*")
-    # tree = construct_tree("a(a|b)*")
-    # .#.*(.*(|ba)a)a
-    # tree = construct_tree("a(a(a|b)*)*")
-    # tree = AbstractSyntaxTree("(a|b)*abb")
-    # tree = AbstractSyntaxTree("a(a|b)*a")
-    # tree = AbstractSyntaxTree("aa*(bb*aa*b)*")
-    tree = AbstractSyntaxTree("(&|b)(ab)*(&|a)")
-    # tree = AbstractSyntaxTree("((a|b)|&)(a|b)*aa")
-    print(tree.in_order())
+    app = Controller()
+    app.run()
