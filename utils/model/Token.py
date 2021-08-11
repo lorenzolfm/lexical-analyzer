051cae91a8c7336475c7df1e239bbf406e095228
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-11 14:22:22 -0300

    token tem um atributo

--- utils/model/Token.py
+++ utils/model/Token.py
@@ -1,15 +1,9 @@
-id_ = 0
-
+from typing import Optional
 
 class Token:
-    def __init__(self, name: str) -> None:
-        global id_
+    def __init__(self, name: str, attribute: Optional[str] = None) -> None:
         self._name = name
-        self._id = id_
-        id_ += 1
+        self._attribute: Optional[str] = attribute
 
     def get_name(self) -> str:
         return self._name
-
-    def get_id(self) -> int:
-        return self._id
