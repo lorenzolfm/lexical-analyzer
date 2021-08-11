76d0aec954e686666fddf349b4fe703bb879482d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2021-08-11 14:24:36 -0300

    typehint

--- utils/model/Token.py
+++ utils/model/Token.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 class Token:
     def __init__(self, name: str, attribute: Optional[str] = None) -> None:
-        self._name = name
+        self._name: str = name
         self._attribute: Optional[str] = attribute
 
     def get_name(self) -> str:
