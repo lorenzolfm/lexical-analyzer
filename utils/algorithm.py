6285c782a6ded7d6f4c8987342f924ba18df81d5
Gabriel Carneiro <carneiro.gabr@gmail.com>
2021-08-13 09:16:33 -0300

    doc algorithm

--- utils/algorithm.py
+++ utils/algorithm.py
@@ -6,6 +6,15 @@ from .model.State import State
 
 
 def get_key_by_value(dict_: dict, data):
+    """
+
+    Args:
+        dict_: Generic dictionary.
+        data: value of the dictionary.
+
+    Returns:
+        Key of the data.
+    """
     for key, value in dict_.items():
         if value == data:
             return key
@@ -14,6 +23,15 @@ def get_key_by_value(dict_: dict, data):
 
 
 def automata_union(automatas: List[FiniteAutomata]) -> FiniteAutomata:
+    """
+
+    Args:
+        automatas: list of finite automata.
+
+    Returns:
+        One finite automata.
+
+    """
     if len(automatas) == 1:
         return automatas[0]
 
